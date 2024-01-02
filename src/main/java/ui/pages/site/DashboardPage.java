2b9b97aea1aca2295a4cecb7de433070bb2bf66f
iamorozov <maadslayer@gmail.com>
2024-01-02 12:07:48 +0300

    add

---
+++ src/main/java/ui/pages/site/DashboardPage.java
@@ -0,0 +1,28 @@
+package ui.pages.site;
+
+import com.codeborne.selenide.As;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Step;
+import ui.pages.factory.CustomLocator;
+import ui.selectors.CustomSelenideElement;
+
+public class DashboardPage {
+
+    @As("Тайтл Dashboard")
+    @CustomLocator(classStartsWith = "AppHeader-context-item-label")
+    private CustomSelenideElement title;
+
+    @As("Поиск")
+    @CustomLocator(attributeName = "data-target", attributeValue = "qbsearch-input.inputButton")
+    private CustomSelenideElement searchInput;
+
+    public static DashboardPage init() {
+        return Selenide.page(DashboardPage.class);
+    }
+
+    @Step("[Проверка] Тайтл на странице Dashboard содержит слово Dashboard")
+    public DashboardPage checkDashboardTitle(){
+        title.checkElementMatchText("^.*Dashboard.*$");
+        return this;
+    }
+}
--- src/test/java/ui/CheckUiTests.java
+++ src/test/java/ui/CheckUiTests.java
@@ -1,7 +1,15 @@
 package ui;
 
+import org.testng.annotations.Test;
+import ui.pages.site.MainPage;
+
 public class CheckUiTests {
 
+    @Test(description = "Поиск репозитория - имя репозитория")
     public void checkUi() {
+        MainPage.openMainPage()
+                .goToSignInPage()
+                .signIn("usefulrepository", "TrueLVL010492")
+                .checkDashboardTitle();
     }
 }
