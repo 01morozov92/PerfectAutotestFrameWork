b5378617e639bcd762f59bc2930721170763bf90
iamorozov <maadslayer@gmail.com>
2024-01-02 12:05:08 +0300

    fix

--- src/test/java/ui/CheckUiTests.java
+++ src/test/java/ui/CheckUiTests.java
@@ -1,24 +1,15 @@
 package ui;
 
-import api.clients.reositories.RepositoryClient;
-import api.clients.reositories.UserRepositoryClient;
-import api.models.repositories.getuserrepository.response.GetUserRepositoryModel;
-import org.assertj.core.api.Assertions;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
-import reporting.ReportStep;
 import ui.pages.site.MainPage;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static api.clients.AbstractGithubClient.MAIN_CONFIG;
 
 public class CheckUiTests {
 
     @Test(description = "Поиск репозитория - имя репозитория")
     public void checkUi() {
         MainPage.openMainPage()
-                .goToSignInPage();
+                .goToSignInPage()
+                .signIn("usefulrepository", "TrueLVL010492")
+                .checkDashboardTitle();
     }
 }
