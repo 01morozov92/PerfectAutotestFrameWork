4a4965a8195d1f92de9ca4fc27ea7c70bbae2182
iamorozov <maadslayer@gmail.com>
2024-01-02 12:06:54 +0300

    delete

--- src/test/java/ui/CheckUiTests.java
+++ src/test/java/ui/CheckUiTests.java
@@ -1,15 +1,7 @@
 package ui;
 
-import org.testng.annotations.Test;
-import ui.pages.site.MainPage;
-
 public class CheckUiTests {
 
-    @Test(description = "Поиск репозитория - имя репозитория")
     public void checkUi() {
-        MainPage.openMainPage()
-                .goToSignInPage()
-                .signIn("usefulrepository", "TrueLVL010492")
-                .checkDashboardTitle();
     }
 }
